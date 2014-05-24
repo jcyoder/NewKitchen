56c893f12adbee4e4a30b878d36f99d76e45708d
jcyoder <kajcyoder@comcast.net>
2014-05-24 11:20:54 -0600

    added click handlers to control menu tab look and feel

--- menu.js
+++ menu.js
@@ -1,2 +1,16 @@
 /*  Menu JavaScript */
 
+$(document).ready( function() {
+    
+    var menutab = 'breakfast';
+    
+    $('ul.navb a#' + menutab ).addClass('chosen');
+    
+    
+    $('ul.navb a').on('click', function(e) {
+			menutab = $(this).attr('id');
+			$('ul.navb a').removeClass('chosen');
+			$(this).addClass('chosen');
+    })
+ 
+});
