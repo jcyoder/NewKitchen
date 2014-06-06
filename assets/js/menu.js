0849c3402b1624a4c09414c9525ec5a604dd5a7d
jcyoder <kajcyoder@comcast.net>
2014-06-05 18:41:55 -0600

    took out iframe and now using ajax calls

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,16 +1,44 @@
 /*  Menu JavaScript */
 
-$(document).ready( function() {
+var MenuModule = function () {};
+
+$(document).ready(function () {
     
-    var menutab = 'breakfast';
+    MenuModule.prototype.init = function () {
+        /* this is a reference to the instance of the MenuModule class created below*/
+        var self = this;
+        this.menutab = 'breakfast';
+        var currenttab = $('ul.navb a#' + this.menutab).addClass('chosen');
+        var tabhref = this.ajax(currenttab.attr('href'));
+
+        $('ul.navb a').on('click', function (e) {
+            /*anchor has a native handler - we are cancelling the native handler*/
+            e.preventDefault();
+            self.handleclick($(this));
+        });
+    };
     
-    $('ul.navb a#' + menutab ).addClass('chosen');
+    /* $el $ means wrapped in jQuery, el stands for element */
+    MenuModule.prototype.handleclick = function ($el) {
+        this.menutab = $el.attr('id');
+        $('ul.navb a').removeClass('chosen');
+        $el.addClass('chosen');
+        this.ajax($el.attr('href'));
+    };
+    MenuModule.prototype.ajax = function (url) {
+        var self = this;
+        $.ajax({url: url, success: function (data) {
+            self.handleajax(data);
+        }
+            });
+    };
     
-    
-    $('ul.navb a').on('click', function(e) {
-			menutab = $(this).attr('id');
-			$('ul.navb a').removeClass('chosen');
-			$(this).addClass('chosen');
-    })
- 
+    MenuModule.prototype.handleajax = function (data) {
+        /* $data is the same as saying var data - just reminding myself that it is a jQuery object*/
+        var $data =  $(data);
+        console.log($data);
+        $('#contentpane').html($data.html());   
+    };
+    var menuModule = new MenuModule();
+    menuModule.init();
 });
